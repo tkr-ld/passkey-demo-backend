aae35e89464cd275c33ce5f85eabd48e35782603
tkr-ld <sonobetakuro@team-lab.com>
2025-03-13 00:02:30 +0900

    パスキー作成エンドポイントを作成

---
+++ registration/app.ts
@@ -0,0 +1,35 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+/**
+ *
+ * Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *
+ */
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    try {
+        const body = JSON.parse(event.body as string);
+        console.log(body);
+        return {
+            headers: {
+                'Access-Control-Allow-Origin': '*', // 全てを許可してしまうので、本来はよろしくない
+            },
+            statusCode: 200,
+            body: JSON.stringify({
+                message: 'OK',
+            }),
+        };
+    } catch (err) {
+        console.log(err);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({
+                message: 'some error happened',
+            }),
+        };
+    }
+};
