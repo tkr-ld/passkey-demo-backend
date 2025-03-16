fbfc8534e9b69f2ce327976b2837eecfb92b9297
tkr-ld <sonobetakuro@team-lab.com>
2025-03-16 15:26:02 +0900

    パスキー作成が成功するようにした

--- registration/app.ts
+++ registration/app.ts
@@ -1,3 +1,7 @@
+import { DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient, PutCommand, PutCommandInput } from '@aws-sdk/lib-dynamodb';
+import { RegistrationResponseJSON, verifyRegistrationResponse } from '@simplewebauthn/server';
+import { isoBase64URL } from '@simplewebauthn/server/helpers';
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 /**
@@ -12,8 +16,68 @@ import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
     try {
-        const body = JSON.parse(event.body as string);
-        console.log(body);
+        const credentials: RegistrationResponseJSON = JSON.parse(event.body as string);
+        // ヘッダーからチャレンジを取得
+        const challenge = event.headers['Challenge'] as string;
+        console.log(credentials);
+        // 認証情報の検証
+        const { verified, registrationInfo } = await verifyRegistrationResponse({
+            response: credentials,
+            expectedChallenge: challenge,
+            expectedOrigin: 'http://localhost:5173',
+            // PRID(ドメイン)
+            expectedRPID: 'localhost',
+            // requireUserVerification: ユーザー検証が必要かどうかを指定する
+            // falseの場合、認証器はユーザーの存在を確認するだけで、特定のユーザーが認証器を操作していることを検証しない
+            requireUserVerification: false,
+        });
+
+        if (!verified) {
+            return {
+                statusCode: 401,
+                body: JSON.stringify({
+                    message: 'verification failed',
+                }),
+            };
+        }
+
+        if (!registrationInfo) {
+            return {
+                statusCode: 400,
+                body: JSON.stringify({
+                    message: 'registrationInfo is not found',
+                }),
+            };
+        }
+        const { credential, aaguid } = registrationInfo;
+
+        const publicKey = isoBase64URL.fromBuffer(credential.publicKey);
+
+        // TODO 同期パスキーであるかの判定
+
+        // DynamoDBにデータを登録する
+        const dynamodbClient = new DynamoDBClient({
+            endpoint: 'http://dynamodb:8000',
+            region: 'ap-northeast-1',
+            credentials: { accessKeyId: 'dummy', secretAccessKey: 'dummy' },
+        });
+        const docClient = DynamoDBDocumentClient.from(dynamodbClient);
+        const params: PutCommandInput = {
+            TableName: 'passkeyPublicKey',
+            Item: {
+                id: credential.id,
+                publicKey,
+                aaguid,
+                registerTime: new Date().toISOString(),
+                last_used: null,
+                // TODO ユーザーIDをどうするか
+                //user_id: 'user1',
+            },
+        };
+
+        const command = new PutCommand(params);
+        await docClient.send(command);
+
         return {
             headers: {
                 'Access-Control-Allow-Origin': '*', // 全てを許可してしまうので、本来はよろしくない
